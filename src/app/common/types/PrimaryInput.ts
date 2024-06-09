14e7160281516f13d705186b7fceb4c3d4dfaa2f
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-06-10 08:26:27 +0900

    チャクラUIに変更

--- src/app/common/types/PrimaryInput.ts
+++
@@ -1,7 +0,0 @@
-export type PrimaryInputProps = {
-  placeholder: string;
-  type: string;
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  className?: string;
-};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import { LoginPage } from "@/features/Login/components/template/LoginPage";
-
 export default function Home() {
   return (
-    <LoginPage />
+    <></>
   );
 }
