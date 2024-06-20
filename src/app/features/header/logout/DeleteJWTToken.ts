0585ac58551daf680fa2ed1156a87f5042f6f98b
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-06-21 00:20:58 +0900

    ログアウト処理作成

---
+++ src/app/features/header/logout/DeleteJWTToken.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export const DeleteJWTToken = async () => {
+  /**
+   * ログアウト処理
+   * サーバー側でJWTトークンの削除を行う
+   * 
+   * Returns:
+   * 200: ログアウト成功
+   * 401: ログアウト失敗
+   */
+  try {
+    const response = await axios.delete(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/v1/logout`,
+      { withCredentials: true }
+    );
+    return response.status;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return error.response?.status;
+    }
+  }
+};
